caab74d79260d3a6cc67477fa78d9ab37a5a460b
Teemu <tehoxd@gmail.com>
2020-11-04 13:24:31 +0200

    Projektin luonti ja komponenttien luonti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { StudiesComponent } from './studies/studies.component';
+import { MeComponent } from './me/me.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    StudiesComponent,
+    MeComponent,
+    HobbiesComponent
   ],
   imports: [
     BrowserModule,
